d0d36faa738b9702c2a5ec5ebebccc3e0e0ef8ec
Shubham12051999 <shubham.navale12@gmail.com>
2020-07-10 12:38:04 +0530

    first commit

---
+++ src/Operations.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sushant
+ */
+interface Operations {
+    void add(double input1, double input2);
+    void sub(double input1, double input2);
+    void mul(double input1, double input2);
+    void div(double input1, double input2);
+    void log(double input2);
+    void InvFact(double input2);
+    void PowRoot(double input1, double input2);
+}
